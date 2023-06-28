1f50ab6e538f2e0e407007d6de438f72e1bd0b60
devanfer02 <ubdevanferrel04@student.ub.ac.id>
2023-06-28 19:34:17 +0700

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+## Learning Algorithm and Data Structure
+
+1. [Freecodecamp Data Structure](https://www.youtube.com/watch?v=B31LgI4Y4DQ) ```C/C++```
+2. [Freecodecamp Pointer](https://www.youtube.com/watch?v=zuegQmMdy8M) ```C/C++```
+
+## Concepts
+
+1. #### [Pointers](./src/Pointers/README.md) (for C/C++ language)
+
+## Scripts
+1. run.sh : to instant compile cpp or c file and put in out dir. Takes 2 parameters, first is the dir inside src and second is the file that has cpp or c extension inside the dir.  
+How to use : just run ```chmod +x ./run.sh``` first. make sure your terminal is bash
+Example    : ```./run.sh Pointers main``` in cli
+
---
+++ run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# CPP/C instant compile file
+
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Please put parameters"
+    echo "First parameter is dir relative to src"
+    echo "Second parameter is the file that want to be compiled"
+    echo "Example: ./run.sh Pointers main"
+    exit 1
+fi
+
+dir="./out/$1"
+file_exe="./out/$1/$2"
+file_c="./src/$1/$2.c"
+file_cpp="./src/$1/$2.cpp"
+
+mkdir -p ./out
+mkdir -p "$dir"
+
+if [ ! -f "$file_exe" ] || ([ ! -f "$file_c" ] && [ ! -f "$file_cpp" ]); then
+    echo "$file_c or $file_cpp doesnt exist"
+    exit 1;
+fi
+
+if [ -f "$file_c" ]; then
+    g++ "$file_c" -o "$file_exe"
+elif [ -f "$file_cpp" ]; then
+    g++ "$file_cpp"-o "$file_exe"
+fi
+
+"$file_exe"
